02a693cd4a9bc460f0baa70c620688e16fc1a573
Jamie Hong <jamie.m.hong@gmail.com>
2016-04-20 00:59:10 -0400

    homework11 updated

--- hw11_2.py
+++ hw11_2.py
@@ -11,7 +11,7 @@ y0 = [1., lbd-2]
 T = 30
 h = 0.05
 ts = np.arange(0.0001, T, h)
-ys = odeint(f, y0, ts)
+ys, infodict = odeint(f, y0, ts, full_output=True, ixpr=True)
 
 plt.figure(figsize=[14,5])
 plt.subplot(121)
@@ -23,7 +23,7 @@ plt.legend(['odeint $y(t)$','odeint $y\'(t)$', 'exact $y(t)$'])
 
 lbd = 50
 y0 = [1., lbd-2]
-ys2 = odeint(f, y0, ts)
+ys2, infodict2 = odeint(f, y0, ts, full_output=True, ixpr=True)
 
 plt.subplot(122)
 plt.plot(ts, ys2[:,:], '.-')
